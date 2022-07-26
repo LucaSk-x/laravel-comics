9f75de2b5a76682088201ec746dd2cfa5b736043
LucaSk-x <lucapiccinno64@gmail.com>
2022-07-26 21:06:42 +0200

    first

---
+++ resources/views/partial/footer.blade.php
@@ -0,0 +1,35 @@
+<footer>
+    <div class="container">
+        <div class="top-footer d-flex position-relative justify-content-between overflow-hidden p-3">
+            <div class="top-footer-left d-flex">
+                <div class="first">
+                    <ul  class="list-unstyled">
+                        <ul class="list-unstyled">
+                            <li><h6 class="text-white text-uppercase">Dc Comics</h6></li>
+                            <li class="text-light"> {{link.name}} </li>
+                        </ul>
+                        <ul class="list-unstyled pt-4">
+                            <li><h6 class="text-white text-uppercase">Shop</h6></li>
+                            <li class="text-light"> {{link.name}} </li>
+                        </ul>
+                    </ul>
+                </div>
+                <div class="second px-4">
+                        <ul class="list-unstyled">
+                            <li><h6 class="text-white text-uppercase">Dc</h6></li>
+                            <li class="text-light"> {{link.name}} </li>
+                        </ul>
+                </div>
+                <div class="third">
+                        <ul class="list-unstyled">
+                            <li><h6 class="text-white text-uppercase">sites</h6></li>
+                            <li class="text-light"> {{link.name}} </li>
+                        </ul>
+                </div>
+            </div>
+            <div class="top-footer-right ms-auto">
+                <img class="dc-logo position-absolute" src="../../public/img/dc-logo-bg.png" alt="">
+            </div>
+        </div>
+    </div>
+</footer>
